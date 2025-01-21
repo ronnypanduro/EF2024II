ea4df4cded7832548ca0965da9bad0d9dfc19db3
Ronny Panduro <100005222+ronnypanduro@users.noreply.github.com>
2025-01-21 14:33:18 -0500

    mspago

---
+++ msventa/src/main/java/msventa/msventa/MsventaApplication.java
@@ -0,0 +1,13 @@
+package msventa.msventa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsventaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsventaApplication.class, args);
+	}
+
+}
---
+++ msventa/src/test/java/msventa/msventa/MsventaApplicationTests.java
@@ -0,0 +1,13 @@
+package msventa.msventa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsventaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
